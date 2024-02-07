6fcdd06f45c9378e8d8a5eca00f2d67fad15c396
francescacallard <francescalily@outlook.com>
2024-02-07 11:27:44 +0000

    commiting

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import './index.css'
 import SearchIcon from '@material-ui/icons/Search'
+import HomeIcon from '@material-ui/icons/Home'
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
 import LinkedInImage from '../../assets/linkedIn.png'
+import HeaderOption from '../HeaderOption'
 
 function Header() {
   return (
     <>
-    <div className='header'></div>
-    
-    <div className='header__left'>
-      <img src={LinkedInImage} />
-
-      <div className="header__search">
-        <SearchIcon />
-      <input name="myInput" placeholder='Search...'></input>
-      </div>
+    <div className='header'>
+     <div className='header__left'>
+       <img src={LinkedInImage} />
+       <div className="header__search">
+         <SearchIcon />
+       <input name="myInput" placeholder='Search...'></input>
+       </div>
       
-
-    </div>
-    <div className='header__right'>
-        
+     </div>
+       <div className='header__right'>
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+     </div>
     </div>
 
 
---
+++ src/components/HeaderOption/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+function HeaderOption( {Icon, title} ) {
+  return (
+    <div classname="headerOption">
+        {Icon && <Icon classname="headerOption__icon"/>}
+        <h3 className='headerOption__title'>{title}</h3>
+        
+    </div>
+  )
+}
+
+export default HeaderOption
