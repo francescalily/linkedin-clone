43ac14724fc72a87645defb55f0619d63936fa81
francescacallard <francescalily@outlook.com>
2024-02-07 17:18:24 +0000

    column styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
 //control spacebar gives us automatic import
-import { Header } from './components';
+import { Header, SideBar } from './components';
 
 
 function App() {
   return (
     <div className="app">
      <Header />
-      {/* App Body */}
-      {/* Sidebar */}
+     <div className="app__body">
+      <SideBar />
+       {/* App Body */}
       {/* Feed */}
       {/* Widgets */}
+     </div>
+     
     </div>
   );
 }
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,9 +1,34 @@
 import React from 'react'
 import './index.css'
+import { Avatar } from '@material-ui/core'
+import backgroundImage from '../../assets/background.png'
+// import HeaderOption from '../HeaderOption'
+// import profileImage from '../../assets/photo.png'
 
 function SideBar() {
   return (
-    <div>SideBar</div>
+    <div className="sidebar">
+        <div className="sidebar__top">
+            <img src={backgroundImage} alt=''/>
+            <Avatar className='sidebar__avatar' />
+            <h2>Franki Callard</h2>
+            <h4>AI Technologist</h4>
+            
+        </div>
+        <div className='sidebar__stats'>
+            <div className="sidebar__stat">
+                <p>Who viewed you</p>
+                <p className="sidebar__statNumber">2,500</p>
+            </div>
+            <div className="sidebar__stat">
+            <p>View on post</p>
+            <p className="sidebar__statNumber">1,200</p>
+            </div>
+        </div>
+        <div className="sidebar_bottom">
+            <p>Recent</p>
+        </div>
+    </div>
   )
 }
 
